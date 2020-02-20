bc846238b4c8300faa56b026f857cbcafb0ae112
krisinacvbn <cvbn@gmail.com>
2020-02-21 01:05:01 +0300

    update

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -17,16 +17,16 @@ urlpatterns = [
     path('topics/', views.topics, name='topics'),
 
     # Страница с подробной информацией по отдельной теме.
-    path('topics/(?P<topic_id>\d+)/', views.topic, name='topic'),
+    path(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
 
     # Страница для добавления новой темы.
     path('new_topic/', views.new_topic, name='new_topic'),
 
     # Страница для добавления новой записи.
-    path('new_entry/(?P<topic_id>\d+)/', views.new_entry, name='new_entry'),
+    path(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry'),
 
     # Страница для редактирования записи.
-    path('edit_entry/(?P<entry_id>\d+)/', views.edit_entry, name='edit_entry'),
+    path(r'^edit_entry/(?P<entry_id>\d+)/$', views.edit_entry, name='edit_entry'),
 ]
 
 app_name ='nameapp'
